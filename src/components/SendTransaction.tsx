02a12f6bf0c73e1ef483de51c98883defcc6f6eb
William Wang <swang45@uw.edu>
2022-06-23 00:48:59 -0700

    Balance Retrived

--- src/components/SendTransaction.tsx
+++ src/components/SendTransaction.tsx
@@ -1,20 +1,27 @@
-import { useConnection, useWallet } from '@solana/wallet-adapter-react';
-import { Keypair, SystemProgram, Transaction, TransactionSignature } from '@solana/web3.js';
-import { FC, useCallback } from 'react';
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import {
+    Keypair,
+    SystemProgram,
+    Transaction,
+    TransactionSignature,
+} from "@solana/web3.js";
+import { FC, useCallback, useState } from "react";
 import { notify } from "../utils/notifications";
 
 export const SendTransaction: FC = () => {
     const { connection } = useConnection();
     const { publicKey, sendTransaction } = useWallet();
+    const [amount, setAmount] = useState<number>();
+    const [receiverAddress, setReceiverAddress] = useState("");
 
     const onClick = useCallback(async () => {
         if (!publicKey) {
-            notify({ type: 'error', message: `Wallet not connected!` });
-            console.log('error', `Send Transaction: Wallet not connected!`);
+            notify({ type: "error", message: `Wallet not connected!` });
+            console.log("error", `Send Transaction: Wallet not connected!`);
             return;
         }
 
-        let signature: TransactionSignature = '';
+        let signature: TransactionSignature = "";
         try {
             const transaction = new Transaction().add(
                 SystemProgram.transfer({
@@ -26,28 +33,72 @@ export const SendTransaction: FC = () => {
 
             signature = await sendTransaction(transaction, connection);
 
-            await connection.confirmTransaction(signature, 'confirmed');
-            notify({ type: 'success', message: 'Transaction successful!', txid: signature });
+            await connection.confirmTransaction(signature, "confirmed");
+            notify({
+                type: "success",
+                message: "Transaction successful!",
+                txid: signature,
+            });
         } catch (error: any) {
-            notify({ type: 'error', message: `Transaction failed!`, description: error?.message, txid: signature });
-            console.log('error', `Transaction failed! ${error?.message}`, signature);
+            notify({
+                type: "error",
+                message: `Transaction failed!`,
+                description: error?.message,
+                txid: signature,
+            });
+            console.log(
+                "error",
+                `Transaction failed! ${error?.message}`,
+                signature
+            );
             return;
         }
     }, [publicKey, notify, connection, sendTransaction]);
 
+    const onChange = (e) => {
+        setAmount(e.currentTarget.value);
+    }
+
     return (
-        <div>
-            <button
-                className="group w-60 m-2 btn animate-pulse disabled:animate-none bg-gradient-to-r from-[#9945FF] to-[#14F195] hover:from-pink-500 hover:to-yellow-500 ... "
-                onClick={onClick} disabled={!publicKey}
-            >
-                <div className="hidden group-disabled:block ">
-                    Wallet not connected
-                </div>
-                <span className="block group-disabled:hidden" > 
-                    Send Transaction 
-                </span>
-            </button>
+        <div className="flex w-full flex-col gap-4 rounded-lg bg-base-300 p-4 shadow-lg">
+            <div className="form-control w-full">
+                <label className="label">
+                    <span className="label-text">Amount</span>
+                </label>
+                <input
+                    className="input-bordered input w-full"
+                    onChange={onChange}
+                    placeholder="Enter amount"
+                    step={0.001}
+                    type="number"
+                    value={amount}
+                />
+            </div>
+            <div className="form-control w-full">
+                <label className="label">
+                    <span className="label-text">Receiver Address</span>
+                </label>
+                <input
+                    className="input-bordered input w-full"
+                    onChange={(e) => setReceiverAddress(e.target.value)}
+                    placeholder="Enter address"
+                    type="text"
+                    value={receiverAddress}
+                />
+            </div>
+            <div>
+                <button
+                    className="group w-60 m-2 btn animate-pulse disabled:animate-none bg-gradient-to-r from-[#9945FF] to-[#14F195] hover:from-pink-500 hover:to-yellow-500 ... "
+                    onClick={onClick}
+                    disabled={!publicKey}>
+                    <div className="hidden group-disabled:block ">
+                        Wallet not connected
+                    </div>
+                    <span className="block group-disabled:hidden">
+                        Send Transaction
+                    </span>
+                </button>
+            </div>
         </div>
     );
 };
--- src/views/transaction/index.tsx
+++ src/views/transaction/index.tsx
@@ -1,8 +1,24 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { SignMessage } from "../../components/SignMessage";
 import { SendTransaction } from "../../components/SendTransaction";
+import useUserSOLBalanceStore from "stores/useUserSOLBalanceStore";
+import { clusterApiUrl, Connection, LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { useWallet } from "@solana/wallet-adapter-react";
 
 export const TransactionView: FC = ({}) => {
+    const connection = new Connection(clusterApiUrl("devnet"));
+    const { publicKey } = useWallet();
+    const [balance, setBalance] = useState(0);
+
+    useEffect(() => {
+        if (!connection || !publicKey) {
+            return;
+        }
+        connection.getAccountInfo(publicKey).then((info) => {
+            setBalance(info.lamports);
+        });
+    }, [connection, publicKey]);
+
     return (
         <div className="md:hero mx-auto p-4">
             <div className="md:hero-content flex flex-col">
@@ -10,8 +26,12 @@ export const TransactionView: FC = ({}) => {
                     Transaction
                 </h1>
                 {/* CONTENT GOES HERE */}
+                <p>
+                    {publicKey
+                        ? `Current Balance: ${balance / LAMPORTS_PER_SOL} SOL`
+                        : ""}
+                </p>
                 <div className="text-center">
-                    <SignMessage />
                     <SendTransaction />
                 </div>
             </div>
